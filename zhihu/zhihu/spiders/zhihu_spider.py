2f31470e0e12664ffbc45ac7947b6d217a7bfafd
WeifanWong <weifanw_stu@163.com>
2018-11-14 19:32:26 +0800

    整理了一下知乎爬取用户信息的代码
    存在的问题是，存在粉丝列表的丢失问题，大部分页码会少爬一个用户的数据

--- zhihu/zhihu/spiders/zhihu_spider.py
+++ zhihu/zhihu/spiders/zhihu_spider.py
@@ -13,17 +13,13 @@ import time
 #capsion_ticket 与 z_c0是两个会更改的参量
 
 class ZhLoginSpider(scrapy.Spider):
-	name = 'zhihu_login'
-# #capsion_ticket 与 z_c0是两个会更改的参量
-
+	name = 'zhihu_spider2'
 
 	cookies={
 	'_gads':'ID=6e8f432804cef60c:T=1541602740:S=ALNI_MY3N0Qg8ks3k6pe3zHhUKSaAvNCnA',
 	'_zap':'0cf72f5d-dc99-40d8-9cca-451a23e81323',
 	'_xsrf':'myD3YXkHGE6Qw0z1xdAcvCuuFG1tWNwR',
-	'cap_id':'"YzA2NzZjN2RkZTU2NDI5Mzg1YzMwZmFjMWJiNTY0OWY=|1541636683|d368a4221ccbd7745d4f01677b0662e32d78c806"',
-	#'capsion_ticket':"2|1:0|10:1541652226|14:capsion_ticket|44:NzcyOWExNWFlYTFjNDk3ZTgyMzU2ODI3MzBhZjJkMjU=|387a0118714705538ea998819ea154cd996c37b9e84ca039e4d746d118385055",
-	#'"2|1:0|10:1541642698|14:capsion_ticket|44:NjJkNTg2N2IwZDJjNDI4MGIxMzc4YzgxNGFjMzBhOGE=|01c8fd6861ed3c4b13fb246392066101d35087181f8881710990dad4b73cf2ea"',
+	'cap_id':'"YzA2NzZjN2RkZTU2NDI5Mzg1YzMwZmFjMWJiNTY0OWY=|1541636683|d368a4221ccbd7745d4f01677b0662e32d78c806"',	#'"2|1:0|10:1541642698|14:capsion_ticket|44:NjJkNTg2N2IwZDJjNDI4MGIxMzc4YzgxNGFjMzBhOGE=|01c8fd6861ed3c4b13fb246392066101d35087181f8881710990dad4b73cf2ea"',
 	'd_c0':'"ALDnGDCudw6PTuSQ2Y9Ovf2VKEZWLbtizEQ=|1541329785"',
 	'l_cap_id':'"ZmM4Y2U3ODEwNDI5NDdmNmFlZDgwYmNjYWNiMDQ0MTc=|1541636683|4369e0e7865af5bdf7d49965ff9755ea6ec3f40f"',
 	'l_n_c':'1',
@@ -31,59 +27,20 @@ class ZhLoginSpider(scrapy.Spider):
 	'q_c1':'39108a0da0c745718940ad289a5e608f|1541329802000|1541329802000',
 	'r_cap_id':'"Mzg2YTIwOGNiOTVjNDY4YjljYWEzNWFiYWIwMDQyZGU=|1541636683|a1696f8a3f3ed4d81de283859a3ac0cf42acb0f5"',
 	'tst':'r',
-	#'z_c0': '"2|1:0|10:1541652241|4:z_c0|92:Mi4xQ2VDLUJBQUFBQUFBc09jWU1LNTNEaVlBQUFCZ0FsVk5FUTNSWEFCU1FEZXZUTkZ6c2xQUXc3bFFteHZOOWRWMjhB|3140b96820fce9db9f6ac58db04d280429779edd80e9a597800f92a71f3fbf92"',
-	#'"2|1:0|10:1541642717|4:z_c0|92:Mi4xQ2VDLUJBQUFBQUFBc09jWU1LNTNEaVlBQUFCZ0FsVk4zZWZRWEFETkwxVW1GUnFWLWEtTXh1UWlOM3F5YjJSYzN3|a887da3e3e4f331eefccdfe64bb8366455e220c216fd74b4970a4249ffee0b83"',
-}
+			}
 
 	headers = {
-#     ':authority': 'www.zhihu.com',
-# ':method': 'POST',
-# ':path': '/api/v3/oauth/sign_in',
-# ':scheme': 'https',
-# 'accept': '*/*',
-# 'accept-encoding': 'gzip, deflate, br',
-# 'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8',
-# 'content-type': 'application/x-www-form-urlencoded',
-# 'dnt': '1',
-# 'origin': 'https://www.zhihu.com',
-# 'referer': 'https://www.zhihu.com/signup?next=%2F',
-# 'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36',
-#      ':authority': 'www.zhihu.com',
-# ':method': 'GET',
-# ':path': '/',
-# ':scheme': 'https',
-# 'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-# 'accept-encoding': 'gzip, deflate, br',
-# 'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8',
-# #'cache-control': 'max-age=0',
-#'content-type': 'application/x-www-form-urlencoded',
-# 'dnt': '1',
-# #'origin': 'https://www.zhihu.com',
-# 'upgrade-insecure-requests': '1',
-#'referer': 'https://www.zhihu.com/signup?next=%2F',
-'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36',
+
+				'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36',
     
-    }
-#     HEADERS = {
-#     'Host': 'www.zhihu.com',
-#     'Referer': 'https://www.zhihu.com/',
-#     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'
-# }
+    		}
 
 	def start_requests(self):
-		self.flag = 0
-		#url = 'https://www.zhihu.com/'
 		url = 'https://www.zhihu.com/people/fei-ming-39-77/activities'
-		url = 'https://www.zhihu.com/people/supergo/activities'
-		#url = 'https://www.zhihu.com/people/lishangyuan/activities'
-		# url = 'https://www.zhihu.com/people/ggg-ah/activities'
-		#url = 'https://www.zhihu.com/people/li-shuai-mai-mai-ti/activities'
-	# 	formdata = {
-	# #'ToOk0KPwMtBtNXtzltBlMKpxBduq0PQzppOk0KPwMtBtNX0x1hBlMK6z8p9a01AzwLAlMK-pRsNa0oslkgrhcLolMsabR58lvkNkbHolMsabIgNkggslPGohbhbbMsapks8k0KKlSwNdMkNojs8k0K5jMwN3Ps8k1gslPGoh-lbbMs7kjs8k0Cc2I0AtKXd2k-BzDH5kPgbfbhrljsapPK5k0orbbhrljsakKG5oPsNa0cOkkgrhMGolMsavU0Q1A2uzDH5kPgbfa2rljsapNKpwClurKdQwngrhLGKkOsNbBTuwodRxI6P2clbf9hRzmlR1609lMsaaLcrklorlNG_mLwr_ahrljsapNKpxHdQtMtQ1kx8k0K5xMseu6lbl20rxMK_xOoeaTwemloukT9Kw8-b_Qcbl_c8xROKkMsu4Nsqxjtf262PxDlRbMs7w12BjPdtyCDRdHXQwXlrh0D01PXAqQgrhj9blSComTkNbLorlgoOk09sz6pBq0Puylx8k0O01J2BqNde1Xlrh00s3OXtrIdA13x8k095k0-NbLkrxgcAl0-owNgu_Albl-crwQWKwRwev8lNwXlrh9-PvOTevDLQw: '
-	# }
+		# url = 'https://www.zhihu.com/people/supergo/activities'
+		url = 'https://www.zhihu.com/people/chen-sheng-du-ruo-4/activities'
 		yield scrapy.Request(url,cookies=self.cookies,headers = self.headers,callback = self.parse)
-		#resp = requests.get(url,headers=self.headers,cookies=self.cookies)
-		#print(resp.text)
+	
 	def parse(self,response):
 
 		#global followers_list
@@ -212,42 +169,10 @@ class ZhLoginSpider(scrapy.Spider):
 			followers_url = base_url + str(page)
 			yield scrapy.Request(followers_url,cookies=self.cookies,headers = self.headers,meta={'ikey':item,'page':page,'followers_url':followers_url,'followings_url':followings_url},callback = self.followers_list_p,dont_filter=True)
 
-		#print(item)
-		#yield scrapy.Request('https://www.zhihu.com',cookies=self.cookies,headers = self.headers,callback = self.test)
-
-
-	# def followers_list_p(self,response):
-
-	# 	item = response.meta['ikey']
-	# 	followers_script = response.xpath('//script[@id="js-initialData"]/text()').extract()
-	# 	# time.sleep(5)
-	# 	res_str1 = r'"initialState":(.*?)"followingColumnsByUser"'
-	# 	followers_detail = re.findall(res_str1,followers_script[0])[0]
-
-	# 	res_str2 = r'"name":"(.*?)","url"'
-	# 	res_str3 = r'"name":".+'
-	# 	followers_name = re.findall(res_str2,followers_detail)
-
-	# 	if followers_name == []:
-	# 		#print(response.text)
-	# 		lost_page = response.meta['page']
-	# 		yield scrapy.Request(response.url,cookies=self.cookies,headers = self.headers,meta={'ikey':item,'page':lost_page,'followers_url':response.meta['followers_url'],'followings_url':response.meta['followings_url']},callback = self.followers_list_p,dont_filter=True)
-		
-
-
-		
-	# 	item['followers_list'].extend(followers_name)
-
-	# 	if len(item['followers_list']) > (item['max_page_followers'] - 1) * 20:  #当粉丝读取完毕
-	# 		max_page_followings = int(int(item['followings_num'])/20) + 1
-	# 		item['max_page_followings'] = max_page_followings
-	# 		base_url = response.meta['followings_url'] + '?page='
-	# 		for page in range(1,max_page_followings + 1):  
-	# 			followings_url = base_url + str(page)
-	# 			yield scrapy.Request(followings_url,cookies=self.cookies,headers = self.headers,meta={'ikey':item,'followings_url':followings_url,'page':page},callback = self.followings_list_p,dont_filter=True)
 
 	def followers_list_p(self,response):
 		#time.sleep(2)
+
 		item = response.meta['ikey']
 		followers_script = response.xpath('//script[@id="js-initialData"]/text()').extract()
 		# time.sleep(5)
@@ -258,7 +183,7 @@ class ZhLoginSpider(scrapy.Spider):
 		res_str3 = r'"name":".+'
 		res_str4 = r'":"(.*?)","id":"'
 		followers_info = re.findall(res_str2,followers_detail)
-
+		# print(response.meta['page'],len(followers_info))
 		# if len(followers_info) < 2 and followers_info != [] :
 		if followers_info == [] or len(followers_info) == 1:
 			#print(response.text)
@@ -275,9 +200,9 @@ class ZhLoginSpider(scrapy.Spider):
 				item['followers_list'].extend([[ele_name,ele_url]])
 			else:
 				continue
-			# item['followers_list']['followers_url'].extend(ele_url)
-
-		if len(item['followers_list']) > (item['max_page_followers'] - 1) * 20:  #当粉丝读取完毕
+		# print(len(item['followers_list']))
+		# print(item['max_page_followers'])
+		if len(item['followers_list']) > (item['max_page_followers'] - 2) * 20:  #当粉丝读取完毕
 			max_page_followings = int(int(item['followings_num'])/20) + 1
 			item['max_page_followings'] = max_page_followings
 			base_url = response.meta['followings_url'] + '?page='
@@ -304,7 +229,7 @@ class ZhLoginSpider(scrapy.Spider):
 		res_str5 = r'"headline":".+'
 		res_str6 = r'","urlToken":"(.*?)","id":"'
 #or len(followings_info) < 20
-		if followings_info == [] or len(followings_info) <7:
+		if followings_info == [] or len(followings_info) <2:
 			lost_page = response.meta['page']
 			yield scrapy.Request(response.url,cookies=self.cookies,headers = self.headers,meta={'ikey':item,'page':lost_page,'followings_url':response.meta['followings_url']},callback = self.followings_list_p,dont_filter=True)
 		#这里的dont filter特别重要，因为scrapy会自动过滤重复请求！
